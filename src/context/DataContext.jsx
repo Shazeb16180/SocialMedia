c1360a358c2cbfdd0ce9f94e8acf1e3c51e790e1
shazeb-fissionhq <shazeb.khan@fissionlabs.com>
2023-06-24 18:41:54 -0700

    feat:added loader

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -4,12 +4,14 @@ import { useEffect } from "react";
 import { useContext } from "react";
 import { dataReducer, initalState } from "../reducer/DataReducer";
 import { AuthContext } from "./AuthContext";
+import { useState } from "react";
 
 export const DataContext = createContext();
 
 export function DataContextProvider({ children }) {
   const { token } = useContext(AuthContext);
   const [state, dispatch] = useReducer(dataReducer, initalState);
+  const [loader, setLoader] = useState(false);
   useEffect(() => {
     (async () => {
       try {
@@ -23,7 +25,6 @@ export function DataContextProvider({ children }) {
       }
     })();
     (async () => {
-      console.log("usss");
       try {
         const response = await fetch("/api/users/bookmark", {
           method: "POST",
@@ -40,10 +41,8 @@ export function DataContextProvider({ children }) {
       }
     })();
   }, [token]);
-  console.log(state.posts);
-  console.log(state.users);
   return (
-    <DataContext.Provider value={{ state, dispatch }}>
+    <DataContext.Provider value={{ state, dispatch, loader, setLoader }}>
       {children}
     </DataContext.Provider>
   );
